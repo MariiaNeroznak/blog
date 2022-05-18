7b56a6388ab45a073088a74c9d86a2e4ce29d751
Mariia <mariia.neroznak@visma.com>
2022-05-18 14:49:30 +0300

    memoized

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import reducers from './reducers';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+
+console.clear();
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
